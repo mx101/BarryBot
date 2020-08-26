203d3cb0cacf6d5fda4b499cf08791f37d393b6d
Michael Xiang <mx9@illinois.edu>
2020-08-26 17:20:15 -0500

    Initial Setup, proof of life, BarryBot can speak

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import discord
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    barryisms = [
+        '#think',
+        'along the way...',
+        'Hey Band!',
+        'Birthday!'
+    ]
+
+    if message.content == '!Barryism':
+        response = random.choice(barryisms)
+        await message.channel.send(response)
+
+client.run(TOKEN)
